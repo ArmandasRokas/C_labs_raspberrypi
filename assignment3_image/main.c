b492da7ddfd0b1a7d97f42657600e8c3f8fdf5d7
Armandas Rokas <armandas.rokas@gmail.com>
2019-04-19 11:30:46 +0200

    refactoring assignment 3

--- assignment3_image/main.c
+++ assignment3_image/main.c
@@ -3,20 +3,12 @@
 #include <stdlib.h>
 #include "opencv/cv.h"
 #define SIZE 256
-
+void countIntensities(int * intensities, IplImage* image);
+int countNumOfDistinctIntensities(int * intensities);
+int writeToFile(int * intensities, IplImage* image, int numOfDistinctIntensities);
 
 int main(int argc, char** argv)
 {
-	FILE * fptr;
-	fptr = fopen("HISTOGRAM.SOL", "w");
-	if(fptr == NULL){
-			printf("File does not exists \n");
-			return -1;
-	}
-
-	int intensities[SIZE] = {0};
-	int numOfDistinctIntensities;
-	
 	printf("Arguments were passed to main():\n");
 	for(int i=0; i<argc; i++){
 		printf("argv[%d] = %s \n", i , argv[i]);
@@ -24,27 +16,53 @@ int main(int argc, char** argv)
 	
 	if( argc != 2)
     {
-        printf( "Unable to load the image\n"
-                "Pass it as the first parameter, e.g.: lab20 typewriter.jpg\n" );
+        printf( "Unable to load the image\n" );
         return -1;
     }
     
-    IplImage* color_image = cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR);  // color image CV_LOAD_IMAGE_COLOR
+    IplImage* color_image = cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR); 
+    if(color_image == NULL){
+		printf("Unable to load the image\n");
+		return -1;
+	}
     IplImage* image = cvCreateImage(cvGetSize(color_image), 8, 1);
     cvCvtColor(color_image, image, CV_RGB2GRAY ); 
-    int numTotalPixels = image->height*image->width;
+
+	int intensities[SIZE] = {0};
+	countIntensities(intensities, image);
+	int numOfDistinctIntensities = countNumOfDistinctIntensities(intensities);
+	writeToFile(intensities, image, numOfDistinctIntensities);
+	
+	return 0;
+}
+
+void countIntensities(int * intensities, IplImage* image){
+	int numTotalPixels = image->height*image->width;
     
     for(int i = 0; i < numTotalPixels; i++){
 			int currIntensity = image->imageData[i];
 			intensities[currIntensity]++; // uses current intensity value as an index 
 	}
-
-	// Counts distinct intensities
+}
+int countNumOfDistinctIntensities(int * intensities){
+	int numOfDistinctIntensities = 0;
 	for(int i = 0; i < SIZE; i++){
 		if(intensities[i] != 0){
 			numOfDistinctIntensities++;
 		}
 	}
+	return numOfDistinctIntensities;
+}
+
+int writeToFile(int * intensities, IplImage* image, int numOfDistinctIntensities){
+	
+	FILE * fptr;
+	fptr = fopen("HISTOGRAM.SOL", "w");
+	if(fptr == NULL){
+			printf("Could not open HISTOGRAM.SOL\n");
+			return -1;
+	}
+	
 	fprintf(fptr, "%d\n", numOfDistinctIntensities);   // Writes distinct intensities		    
 	fprintf(fptr, "%dx%d\n", image->width, image->height); // Writes resolution
 	
@@ -54,6 +72,5 @@ int main(int argc, char** argv)
 		}
 	}
 	fclose(fptr);
-    
-	return 0;
+	return 1;
 }
