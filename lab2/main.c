f2b52ca369dc993051366e6fb6c168128b8b8ca3
Armandas Rokas <armandas.rokas@gmail.com>
2023-08-15 19:27:00 +0200

    added new line

--- lab2/main.c
+++ lab2/main.c
@@ -115,7 +115,7 @@ int main(int argc, char **argv)
 	{
         usleep(200000); //delay for 0.2 seconds
      	unsigned int pin_value = *(gpio + 13) >> gSwitch; // read pin state (no debounce to make code more readable)
-		printf("%u", pin_value &= 1);
+		printf("%u\n", pin_value &= 1);
         if (pin_value &= 1 == 1)
 		{
 			printf("Button pushed"); // print message
